3f59af367b2c9c6b0166c440e5f2f3706a659781
kathryn-sanders <kathryn.sanders.ks@gmail.com>
2019-11-09 20:31:21 -0500

    initial frontend setup

--- gw2UpgradeChecker.js
+++ gw2UpgradeChecker.js
@@ -11,9 +11,9 @@ gw2api.authenticate("D2B5389F-F40A-4547-9D2C-FAC66DACEB63374FC165-8917-4A24-9DB0
 // }) 
 
 
-gw2api.account().bank().get().then((bank) =>{
+gw2api.account().bank().get().then((bank) => {
     // console.log(bank);
-    
+
     let itemsToCheckRarity = new Set();
     let equipUpgrades = {};
     const ectoItemId = 19721;
@@ -25,40 +25,40 @@ gw2api.account().bank().get().then((bank) =>{
     itemsToCheckTP.push(ectoItemId);
 
     bank.forEach(item => {
-        if(itemIsTradableWithUpgrades(item)){
+        if (itemIsTradableWithUpgrades(item)) {
             itemsToCheckRarity.add(item.id);
-            itemsToCheckRarity.add(item.upgrades[0]);     
-        };        
+            itemsToCheckRarity.add(item.upgrades[0]);
+        };
     });
 
     gw2api.items().many(Array.from(itemsToCheckRarity)).then((itemDetails) => {
         // console.log(itemDetails);
 
         itemDetails.forEach(item => {
-            if(!itemIsUpgradeComponent(item) && itemIsExotic(item)){            
+            if (!itemIsUpgradeComponent(item) && itemIsExotic(item)) {
                 itemsToCheckTP.push(item.id);
                 itemsToCheckTP.push(item.details.suffix_item_id);
-                equipUpgrades[item.id]=item.details.suffix_item_id;
+                equipUpgrades[item.id] = item.details.suffix_item_id;
             };
-            
+
             itemIDtoDetailsDictionary[item.id] = item;
         });
 
         return gw2api.commerce().prices().many(itemsToCheckTP)
-        .then((priceInfo) => {
-            return {
-                priceInfo,
-                itemIDtoDetailsDictionary,
-            };
-        });
+            .then((priceInfo) => {
+                return {
+                    priceInfo,
+                    itemIDtoDetailsDictionary,
+                };
+            });
 
-    }).then(({priceInfo, itemIDtoDetailsDictionary}) =>{
+    }).then(({ priceInfo, itemIDtoDetailsDictionary }) => {
 
         // console.log(priceInfo);  
-        
+
         let priceInfoDictionary = {};
 
-        priceInfo.forEach(priceInfoItem =>{
+        priceInfo.forEach(priceInfoItem => {
             priceInfoDictionary[priceInfoItem.id] = priceInfoItem;
         });
 
@@ -71,17 +71,17 @@ gw2api.account().bank().get().then((bank) =>{
 
                 upgrade: itemIDtoDetailsDictionary[upgradeId],
 
-                sellToTP:{
+                sellToTP: {
                     buyPrice: priceInfoDictionary[equipId].buys.unit_price,
                     sellPrice: priceInfoDictionary[equipId].sells.unit_price,
                 },
 
-                extractRareThenSalvage:{
+                extractUpgradeThenSalvage: {
                     buyPrice: priceInfoDictionary[upgradeId].buys.unit_price + (priceInfoDictionary[ectoItemId].buys.unit_price * 1.2),
                     sellPrice: priceInfoDictionary[upgradeId].sells.unit_price + (priceInfoDictionary[ectoItemId].sells.unit_price * 1.2),
                 },
 
-                blackLionSalvage:{
+                blackLionSalvage: {
                     buyPrice: priceInfoDictionary[upgradeId].buys.unit_price + (priceInfoDictionary[ectoItemId].buys.unit_price * 1.66),
                     sellPrice: priceInfoDictionary[upgradeId].sells.unit_price + (priceInfoDictionary[ectoItemId].sells.unit_price * 1.66),
                 },
@@ -90,7 +90,8 @@ gw2api.account().bank().get().then((bank) =>{
             priceCompareData.push(newCompareData);
             console.log(newCompareData);
         });
-    });  
+        displayResultsToTable(priceCompareData)
+    });
 
 });
 
@@ -99,14 +100,39 @@ gw2api.account().bank().get().then((bank) =>{
 // itemsToCheck.add(item.upgrades[0]);
 
 
-function itemIsTradableWithUpgrades(item){
-    return (item && !item.binding && item.upgrades); 
+function itemIsTradableWithUpgrades(item) {
+    return (item && !item.binding && item.upgrades);
 }
 
-function itemIsExotic(item){
+function itemIsExotic(item) {
     return (item.rarity === "Exotic");
 }
 
-function itemIsUpgradeComponent(item){
+function itemIsUpgradeComponent(item) {
     return (item.type === "UpgradeComponent");
 }
+
+
+
+
+
+
+
+
+
+
+function displayResultsToTable(priceCompareData) {
+    //     $("#resultsTbody").append($("<tr>", {html: "hello"}))
+    //     $("<tr>", {html: "hello"})
+
+    priceCompareData.forEach((resultRow) => {
+        let newRow = $("<tr>");
+
+        newRow.append($("<td>", { html: `<img src="${resultRow.equip.icon}"/> ${resultRow.equip.name} <br/> <img src="${resultRow.upgrade.icon}"/> ${resultRow.upgrade.name}` }))
+        newRow.append($("<td>", { html: resultRow.sellToTP.buyPrice}))
+        newRow.append($("<td>", { html: resultRow.extractUpgradeThenSalvage.buyPrice}))
+        newRow.append($("<td>", { html: resultRow.blackLionSalvage.buyPrice}))
+
+        $("#resultsTbody").append(newRow);
+    });
+};
